d8ae4291876ae0b682f5153d05e8fc675234ec6e
Daniel Hernández Alcojor <danielhal@ext.inditex.com>
2024-07-19 14:13:14 +0200

    Working on it

---
+++ src/pages/projects/001-101-projects.md
@@ -0,0 +1,15 @@
+---
+title: '101 Proyectos'
+pubDate: 2024-07-19
+description: '101 proyectos de Daniel H. Alcojor'
+image:
+    url: ''
+    alt: '101 proyectos'
+tags: ["astro", "learning in public"]
+---
+
+# 101 Proyectos
+
+Publicado: 19 de Julio de 2024
+
+Este es un proyecto de **Daniel H. Alcojor** que pretende ser un lugar donde se puedan publicar proyectos de software y arte.
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,52 @@
+import colors from "tailwindcss/colors.js";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			black: colors.black,
+			white: colors.white,
+			primary: {
+				DEFAULT: colors.purple["600"],
+				light: colors.purple["200"],
+				dark: colors.purple["800"],
+			},
+			accent: {
+				DEFAULT: colors.amber["500"],
+				light: colors.amber["200"],
+				dark: colors.amber["800"],
+			},
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+			function ({ addComponents, theme }) {
+				addComponents({
+					'.text-primary': {
+						color: theme('colors.primary.DEFAULT'),
+					},
+					'.text-accent': {
+						color: theme('colors.accent.DEFAULT'),
+					},
+					'.bg-accent': {
+						backgroundColor: theme('colors.accent.DEFAULT'),
+					},
+					'.border-primary': {
+						borderColor: theme('colors.primary.DEFAULT'),
+					},
+					'.border-accent': {
+						borderColor: theme('colors.accent.DEFAULT'),
+					},
+					'.text-gradient': {
+						background: "linear-gradient(to right, theme('colors.primary.DEFAULT'), theme('colors.accent.DEFAULT'))",
+						backgroundClip: "text",
+						"-webkit-text-fill-color": "transparent",
+					},
+					'.bg-gradient': {
+						background: "linear-gradient(to right, theme('colors.primary.DEFAULT'), theme('colors.accent.DEFAULT'))",
+					},
+				});
+			},
+	],
 }
